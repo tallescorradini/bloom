b3c4c8529876663ca0ee01648859130f3eb68123
Talles Corradini <tallescorradini@gmail.com>
2022-01-15 19:30:04 -0300

    feat(Home): add message when list is empty

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import { useEffect, useState } from "react";
 
 import styles from "./Home.module.scss";
 import { getUserGalleries } from "../services/firebase/firebaseDb";
+import illustrationGalleries from "../public/illustration-galleries.svg";
 
 const userId = process.env.NEXT_PUBLIC_USER_ID_PLACEHOLDER;
 const MAX_GALLERY_THUMBNAILS = 5;
 
 export default function Home() {
   const [galleries, setGalleries] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   function formatCreatedAt(milisecondsElapsed) {
     const date = new Date(milisecondsElapsed);
@@ -26,7 +28,10 @@ export default function Home() {
 
   useEffect(() => {
     getUserGalleries({ userId, maxThumbnails: MAX_GALLERY_THUMBNAILS }).then(
-      (galleries) => setGalleries(galleries)
+      (galleries) => {
+        setGalleries(galleries);
+        setLoading(false);
+      }
     );
   }, []);
 
@@ -41,56 +46,64 @@ export default function Home() {
       </header>
 
       <main>
-        <ul className={styles.galleryList}>
-          {galleries.map((gallery) => (
-            <li key={gallery.id}>
-              <Link href={`/${gallery.id}`}>
-                <a>
-                  <section className={styles.gallery}>
-                    <header>
-                      <h2 className={styles.title}>{gallery.name}</h2>
-                      <h3 className={styles.createdAtDate}>
-                        {formatCreatedAt(gallery.createdAt)}
-                      </h3>
-                      <span className={styles.accessLink} aria-hidden="true">
-                        <span
-                          aria-hidden="true"
-                          className="icon xs chevronRight"
-                        ></span>
-                        Ver
-                      </span>
-                    </header>
+        {!loading && isEmpty ? (
+          <div className={styles.emptyGallery}>
+            <img src={illustrationGalleries.src} alt="" width="360" />
+            <h2 className={styles.title}>Nenhuma galeria disponível</h2>
+            <p>Clique no botão pra adicionar sua primeira galeria</p>
+          </div>
+        ) : (
+          <ul className={styles.galleryList}>
+            {galleries.map((gallery) => (
+              <li key={gallery.id}>
+                <Link href={`/${gallery.id}`}>
+                  <a>
+                    <section className={styles.gallery}>
+                      <header>
+                        <h2 className={styles.title}>{gallery.name}</h2>
+                        <h3 className={styles.createdAtDate}>
+                          {formatCreatedAt(gallery.createdAt)}
+                        </h3>
+                        <span className={styles.accessLink} aria-hidden="true">
+                          <span
+                            aria-hidden="true"
+                            className="icon xs chevronRight"
+                          ></span>
+                          Ver
+                        </span>
+                      </header>
 
-                    <ul className={styles.thumbnailList}>
-                      {gallery.images.map((image) => (
-                        <li key={image.id}>
-                          <Image
-                            cloudName={process.env.NEXT_PUBLIC_CLOUD_NAME}
-                            publicId={image.src.cloudinary.publicId}
-                            version={image.src.cloudinary.version}
-                            loading="lazy"
-                          >
-                            <Transformation
-                              gravity="auto"
-                              height="150"
-                              width="150"
-                              crop="fill"
-                            />
-                          </Image>
-                        </li>
-                      ))}
-                      {Array.from(
-                        Array(MAX_GALLERY_THUMBNAILS - gallery.images.length)
-                      ).map((v, i) => (
-                        <li key={i}></li>
-                      ))}
-                    </ul>
-                  </section>
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+                      <ul className={styles.thumbnailList}>
+                        {gallery.images.map((image) => (
+                          <li key={image.id}>
+                            <Image
+                              cloudName={process.env.NEXT_PUBLIC_CLOUD_NAME}
+                              publicId={image.src.cloudinary.publicId}
+                              version={image.src.cloudinary.version}
+                              loading="lazy"
+                            >
+                              <Transformation
+                                gravity="auto"
+                                height="150"
+                                width="150"
+                                crop="fill"
+                              />
+                            </Image>
+                          </li>
+                        ))}
+                        {Array.from(
+                          Array(MAX_GALLERY_THUMBNAILS - gallery.images.length)
+                        ).map((v, i) => (
+                          <li key={i}></li>
+                        ))}
+                      </ul>
+                    </section>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
 
         <Link href={`/add-gallery?totalGalleries=${galleries.length}`}>
           <a aria-label="Criar nova galeria" className={styles.fab}>
